4f9c148fd0c0e1194eff65bdccd7638b3335713a
Falk Ahlendorf <falk.ahlendorf@googlemail.com>
2021-07-03 14:32:05 +0200

    Basic SDL Setup, open a simple window and draw a circle to see that everything works as expected

---
+++ FrameWork.cpp
@@ -0,0 +1,56 @@
+#include "FrameWork.hpp"
+
+#include <SDL.h>
+
+FrameWork::FrameWork(int const width, int const height):
+    height_(height),
+    width_(width),
+    renderer_(nullptr),
+    window_(nullptr)
+{
+
+    SDL_Init(SDL_INIT_VIDEO);
+
+    SDL_CreateWindowAndRenderer(width_,
+                                height_,
+                                0,
+                                &window_,
+                                &renderer_
+                                );
+
+    SDL_SetRenderDrawColor(renderer_, 0,0,0,255);
+    SDL_RenderClear(renderer_);
+    SDL_RenderPresent(renderer_);
+}
+
+FrameWork::~FrameWork()
+{
+    SDL_DestroyRenderer(renderer_);
+    SDL_DestroyWindow(window_);
+    SDL_Quit();
+}
+
+void FrameWork::drawCircle()
+{
+    SDL_SetRenderDrawColor(renderer_, 255, 0, 0, 255);
+
+    for(unsigned i = 0; i<3600; ++i) {
+        SDL_RenderDrawPointF(renderer_,
+                             sinf((i /1800.0)*M_PI)*100 + 320,
+                             cosf((i /1800.0)*M_PI)*100 + 200);
+
+    }
+
+    SDL_RenderPresent(renderer_);
+}
+
+void FrameWork::run( void )
+{
+    SDL_Event event;
+
+    do {
+        drawCircle();
+        SDL_Delay(10); /* Just for now ... */
+        SDL_PollEvent(&event);
+    }while(SDL_QUIT != event.type);
+}
---
+++ FrameWork.hpp
@@ -0,0 +1,11 @@
+#ifndef FRAMEWORK_HPP
+#define FRAMEWORK_HPP
+
+
+class FrameWork
+{
+public:
+    FrameWork();
+};
+
+#endif // FRAMEWORK_HPP
