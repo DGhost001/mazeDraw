60308f687aaef844237fd88707ac9a58cea98f43
Falk Ahlendorf <falk.ahlendorf@googlemail.com>
2021-07-05 21:41:00 +0200

    Basic Maze API

---
+++ Labyrinth.cpp
@@ -0,0 +1,6 @@
+#include "Labyrinth.hpp"
+
+Labyrinth::Labyrinth()
+{
+
+}
---
+++ Labyrinth.hpp
@@ -0,0 +1,63 @@
+#ifndef LABYRINTH_HPP
+#define LABYRINTH_HPP
+
+#include <string>
+#include <array>
+#include <memory>
+#include <cstddef>
+
+
+/* Just a simplification for multidimensional arrays *
+ * see http://cpptruths.blogspot.com/2011/10/multi-dimensional-arrays-in-c11.html
+ **/
+template <class T, size_t ROW, size_t COL>
+using Matrix = std::array<std::array<T, COL>, ROW>;
+
+struct SDL_Renderer;
+class Image;
+
+
+class Labyrinth
+{
+public:
+    enum class MazeContent {
+        Grass,
+        Wall,
+        Waterbottle,
+        Start,
+        Treashure,
+        Count /* Keep this at the END! */
+    };
+
+
+
+    Labyrinth();
+
+    void load(const std::string &path);
+    void save(const std::string &path);
+
+    /**
+     * @brief render Renders the Labyrinth
+     * @param renderer This is the renderer to render to
+     * @param cx the center of the rendered part
+     * @param cy the center of the rendered part
+     * @param w the width of the render window in tiles
+     * @param h the height of the render window in tiles
+     */
+    void render(std::shared_ptr<SDL_Renderer> renderer,
+                size_t const cx,
+                size_t const cy,
+                size_t const w,
+                size_t const h
+                );
+
+    /* Sets a cell in the maze */
+    void set(size_t const x, size_t const y, MazeContent const cell);
+
+private:
+    Matrix<MazeContent, 1000, 1000> maze_;
+    std::array< std::shared_ptr< Image >, static_cast<size_t>(MazeContent::Count)> textures_;
+
+};
+
+#endif // LABYRINTH_HPP
