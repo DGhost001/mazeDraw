460fe29603c7735bf98f7f1fb852d5727a954d90
Falk Ahlendorf <falk.ahlendorf@googlemail.com>
2021-07-11 09:35:18 +0200

    Fixed rendering of the maze

--- Labyrinth.cpp
+++ Labyrinth.cpp
@@ -124,13 +124,22 @@ void Labyrinth::render(std::shared_ptr<SDL_Renderer> renderer,
             ) const
 {
 
-    for(int y = cy - h / 2; y < cy + h /2; ++y) {
-       for(int x = cx - w / 2; x < cx + h /2; ++x) {
-           if( x  > 0 && y > 0 && y < maze_.size() && x < maze_[y].size()) {
-               textures_[indexOf(maze_[y][x])]->render(
+    size_t const h2 = h / 2;
+    size_t const w2 = w / 2;
+
+    for(int y = 0; y < h; ++y) {
+
+        ssize_t const my = y - h2 + cy;
+
+        for(int x = 0; x < w; ++x) {
+
+           ssize_t const mx = x - w2 + cx;
+
+           if( mx  >= 0 && my >= 0 && my < maze_.size() && mx < maze_[my].size()) {
+               textures_[indexOf(maze_[my][mx])]->render(
                            renderer,
-                           (cx - w / 2) * 32,
-                           (cy - h / 2) * 32);
+                           x * 32,
+                           y * 32);
            }
        }
     }
